568451dc16a72f12649b3491ba537cf04a5c9570
AmbaCode33 <ambakiderimokpe@gmail.com>
2023-09-07 10:47:16 -0700

    hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints an outcome
+ *
+ * Return: 0 when a success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
