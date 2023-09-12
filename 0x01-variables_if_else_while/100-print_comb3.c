ee748c123c63a727bb5067a9d3ba7ee08f12cf95
AmbaCode33 <ambakiderimokpe@gmail.com>
2023-09-11 18:40:24 -0700

    invention is a combo of brains and materials

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,20 +12,23 @@ int main(void)
 {
 	int digit1 = 0, digit2;
 
-	while (digit1 <= 9)
+	while (digit1 <= 99)
 	{
-		digit2 = 0;
-		while (digit2 <= 9)
+		digit2 = digit1;
+		while (digit2 <= 99)
 		{
-			if (digit1 != digit2 && digit1 < digit2)
+			if (digit2 != digit1)
 			{
-				putchar(digit1 + 48);
-				putchar(digit2 + 48);
+				putchar((digit1 / 10) + 48);
+				putchar((digit1 % 10) + 48);
+				putchar(' ');
+				putchar((digit2 / 10) + 48);
+				putchar((digit2 % 10) + 48);
 
-				if (digit1 + digit2 != 17)
+				if (digit1 != 98 || digit2 != 99)
 				{
 					putchar(',');
-					putchar(',');
+					putchar(' ');
 				}
 			}
 			digit2++;
